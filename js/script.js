ea76087dcf4752e5d94cb892ca56ccc8126c3296
Agnieszka <aga.murawska@gmail.com>
2017-08-01 12:34:26 +0200

    comments in js file

---
+++ js/script.js
@@ -0,0 +1,16 @@
+﻿// scripts.js
+
+// right attached js file- test
+var variable = 'test';
+console.log(variable);
+
+
+var name = prompt('Podaj swoje imię');
+alert('Hej, ' + name);
+console.log('Hello, ' + name);
+
+//end of the script
+
+/*
+inny komentarz do czegokolwiek
+*/
